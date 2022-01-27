e36b385f432abda6118a9c7726ff61f017e9ba49
Marcin Wiewióra <marcin.wiewiora@QLOGIX.PL>
2022-01-27 10:13:25 +0100

    Another tests 27012022

--- cypress/integration/formsLayoutTestsSet.spec.js
+++ cypress/integration/formsLayoutTestsSet.spec.js
@@ -15,23 +15,18 @@ describe("Forms testings scenarios", () => {
         onFormLayoutPage.submitInlineForm("Marcin", "marcin_wiewiora@wp.pl")
         
         //Using the grid form
-        onNavigation.formsFormLayouts()
         onFormLayoutPage.submitUsingTheGridForm("marcin_wiewiora@wp.pl", "!Password1", true)
         
         //Using Basic form
-        onNavigation.formsFormLayouts()
         onFormLayoutPage.submitUsingBasicForm("marcin_wiewiora@wp.pl", "Password1", true)
         
         //Using form without labels
-        onNavigation.formsFormLayouts()
         onFormLayoutPage.submitFormWithoutLabels("All", "Message to all", "Very urgent message")
         
         //Using block form
-        onNavigation.formsFormLayouts()
         onFormLayoutPage.submitBlockForm("Marcin", "Wiewióra", "marcin_wiewiora@wp.pl", "www.marcinwiewiora.pl")
         
         //Using horizontal form
-        onNavigation.formsFormLayouts()
         onFormLayoutPage.submitHorizontalForm("marcin_wiewiora@wp.pl", "!Password1", true)
     })
 
--- cypress/support/page_objects/datepickerPage.js
+++ cypress/support/page_objects/datepickerPage.js
@@ -1,5 +1,26 @@
 export class DatePicker {
-    
+    //Common Datepicker
+    setCommonDatepicker(day){
+        cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
+            cy.wrap(input).click()
+            cy.wrap(input).get('nb-calendar-day-picker').contains(day).click()
+            cy.wrap(input).invoke('prop', 'value').should('contain', 'Jan '+day+', 2022')
+        })
+    }
+
+    setDatepickerWithRange(dayStart, dayEnd){
+        cy.contains('nb-card', 'Datepicker With Range').find('input').then( input => {
+            cy.wrap(input).click()
+            cy.wrap(input).get('nb-calendar-day-picker').contains(dayStart).click()
+            cy.wrap(input).get('nb-calendar-day-picker').contains(dayEnd).click()
+            cy.wrap(input).invoke('prop', 'value').should('contain', 'Jan '+dayStart+', 2022 - Jan '+dayEnd+', 2022')
+        })
+    }
+
+    //Datepicker With Range
+
+
+    //Datepicker With Disabled Min Max Values
 }
 
-export const onDatePicker
+export const onDatePicker = new DatePicker()
